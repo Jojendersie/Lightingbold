66cffd2a56b00109cb7de6235e555bf82e58cd21
gebert <gebertenrico@googlemail.com>
2013-11-22 22:14:17 +0100

    added dynarray and some changes in the map class added Utilities namespace and Assert functions in the predecl.hpp

--- Lightingbolt/src/predecl.hpp
+++ Lightingbolt/src/predecl.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+typedef unsigned int uint;
+
 namespace Sound {
 	class Sound;
 	class Source;
@@ -14,3 +16,29 @@ namespace Math {
 namespace Map {
 	class Map;
 } // namespace Map
+
+namespace Utilities {
+	class DynArray;
+} // namespace Utilities
+
+#ifdef _DEBUG
+#include <intrin.h>
+
+// Simple assertion like cassert, but stops in the line of source, where the
+// assertion is located in not in some other file like the std::assert.
+#define Assert(a) if(!(a)) __debugbreak()
+
+// The Assert__ execute A (even in release) and test for non-/equality with Const (debug only)
+#define AssertEq(A,Const) if((A)!=(Const)) __debugbreak()
+#define AssertNeq(A,Const) if((A)==(Const)) __debugbreak()
+
+#else	// _DEBUG
+
+
+#define Assert(a)
+
+// The Assert__ execute A (even in release) and test for non-/equality with Const (debug only)
+#define AssertEq(A,Const) A
+#define AssertNeq(A,Const) A
+
+#endif // _DEBUG

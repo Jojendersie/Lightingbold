9936ffe8118e94252add08bab7c88b520c3a3e0f
gebert <gebertenrico@googlemail.com>
2013-11-22 21:09:38 +0100

    added High to map header

--- Lightingbolt/src/map/map.hpp
+++ Lightingbolt/src/map/map.hpp
@@ -11,8 +11,11 @@ namespace Map {
 		/// \brief Computes the position where the ray hits the scene
 		Math::Vec2 RayCast( const Math::Vec2& _position, const Math::Vec2& _direction );
 
-		/// \brief 
+		/// \brief Computes the gradient of the map at the given position
 		Math::Vec2 Gradient( const Math::Vec2& _position );
+
+		/// \breif Returns the hight of the map at the given position
+		float High( const Math::Vec2& _position);
 	private:
 	};
 

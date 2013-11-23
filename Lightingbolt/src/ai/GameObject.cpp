907263501c9d836be011dbfb969a1c201d588780
gebert <gebertenrico@googlemail.com>
2013-11-23 23:23:57 +0100

    move? move!

--- Lightingbolt/src/ai/GameObject.cpp
+++ Lightingbolt/src/ai/GameObject.cpp
@@ -21,6 +21,12 @@ namespace Ai
 		m_goal = Math::Vec2(0);
 		m_position = Math::Vec2(0);
 		m_radius = 0;
+
+		m_timetoReachMaxSpeed = 960.0f;
+		m_maxSpeed = 0.04f;
+		m_friction = 5/m_timetoReachMaxSpeed;
+		m_acceleration = m_maxSpeed*m_friction;
+		m_velocity = 0.0f;
 	}
 
 	void GameObject::setDirection(const Math::Vec2& _newDirection){
@@ -75,12 +81,25 @@ namespace Ai
 
 	void GameObject::update()
 	{
+		//m_velocity += m_acceleration - m_friction*m_velocity;
+		//if(m_velocity.x > m_maxSpeed) m_velocity.x = m_maxSpeed;
+		//if(m_velocity.y > m_maxSpeed) m_velocity.y = m_maxSpeed;
+
 		Math::Vec2 targetDirection = m_goal - m_position;
-		if(targetDirection.length() == 0)
+		if(targetDirection.length() <= 0.005)
+		{
+			m_direction *= 0.6f;
+			m_position += m_direction;
 			return;
+		}
 
-		m_direction = targetDirection/targetDirection.length() *0.0010;// * m_map->Density(m_position);*/ Math::lerp(m_direction,(targetDirection/*targetDirection.length()*/), 0.005);
+		m_direction+=  /*targetDirection/targetDirection.length() *m_velocity;// * m_map->Density(m_position);*/ Math::lerp((m_direction),(targetDirection/targetDirection.length()*0.8), 0.00005f);
 
+		//m_direction = m_direction/m_direction.length() * 0.0009;
+		//m_velocity *= 0.5;
+
+		m_direction *= 0.5;
+		
 		m_position += m_direction;
 
 		//m_position = Math::lerp(m_position,(m_goal/*targetDirection.length()*/), 0.005);

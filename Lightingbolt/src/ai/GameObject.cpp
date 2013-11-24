377aad7e53482e86438574f9ced48f8cb604f0ba
gebert <gebertenrico@googlemail.com>
2013-11-24 10:13:24 +0100

    its time to move in the right direction

--- Lightingbolt/src/ai/GameObject.cpp
+++ Lightingbolt/src/ai/GameObject.cpp
@@ -17,16 +17,14 @@ namespace Ai
 	GameObject::GameObject(Map::Map* _map) : m_map(_map)
 	{
 		m_direction = Math::Vec2(0);
-		//m_energy = 0.5f;
+		m_energy = 1.0f;
 		m_goal = Math::Vec2(0);
 		m_position = Math::Vec2(0);
 		m_radius = 0;
 
-		m_timetoReachMaxSpeed = 960.0f;
-		m_maxSpeed = 0.04f;
-		m_friction = 5/m_timetoReachMaxSpeed;
-		m_acceleration = m_maxSpeed*m_friction;
-		m_velocity = 0.0f;
+		m_accelerationPerFrame = 0.00015f;
+		m_maxAcceleration = 0.0005f;
+		m_friction = 0.975f;
 	}
 
 	void GameObject::setDirection(const Math::Vec2& _newDirection){
@@ -81,28 +79,35 @@ namespace Ai
 
 	void GameObject::update()
 	{
-		//m_velocity += m_acceleration - m_friction*m_velocity;
-		//if(m_velocity.x > m_maxSpeed) m_velocity.x = m_maxSpeed;
-		//if(m_velocity.y > m_maxSpeed) m_velocity.y = m_maxSpeed;
-
-		Math::Vec2 targetDirection = m_goal - m_position;
-		if(targetDirection.length() <= 0.005)
-		{
-			m_direction *= 0.6f;
-			m_position += m_direction;
-			return;
-		}
-
-		m_direction+=  /*targetDirection/targetDirection.length() *m_velocity;// * m_map->Density(m_position);*/ Math::lerp((m_direction),(targetDirection/targetDirection.length()*0.8), 0.00005f);
-
-		//m_direction = m_direction/m_direction.length() * 0.0009;
-		//m_velocity *= 0.5;
-
-		m_direction *= 0.5;
-
-		m_position += m_direction;
+		movement();
 		checkForBoundaries();
-		//m_position = Math::lerp(m_position,(m_goal/*targetDirection.length()*/), 0.005);
+	}
+
+	void GameObject::movement()
+	{
+		//m_direction = m_direction.normalize() * (m_direction.length() *0.9f);
+		// compute current target and how to get there
+		Math::Vec2 difference = m_goal - m_position;
+		Math::Vec2 differenceNormal = difference.normalize();
+
+        float distance = difference.length();
+
+		// every step we throw in some friction, to slow the object down
+		m_direction *= 0.97f;//m_friction;
+
+		Math::Vec2 acceleration = differenceNormal * 0.00015f;
+        //acceleration = acceleration / (distance * distance);
+
+		if(acceleration.length() > m_maxAcceleration) acceleration = acceleration.normalize() * m_maxAcceleration;
+
+		//Math::Vec2 force = m_energy * acceleration;
+		//force *= m_gravityFactor;
+        
+		// that is our new movement
+		m_direction += acceleration;
+
+		// end we wish to move ^^
+		m_position += m_direction;
 	}
 
 	void GameObject::checkForBoundaries()

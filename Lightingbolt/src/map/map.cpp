32f8b53fbea0c61f839e47e7bc02641edcea3cdf
Johannes Jendersie <jojendersie@web.de>
2014-01-07 21:14:39 +0100

    nothing will happen

--- Lightingbolt/src/map/map.cpp
+++ Lightingbolt/src/map/map.cpp
@@ -33,18 +33,20 @@ namespace Map
 	void Map::InitMap()
 	{
 		float amplitude = 5.0f;
+		float offset = 0.5f;
 		// build density map
 		for(int y=0;y<m_height;++y)
 		{
 			for(int x=0;x<m_width;++x)
 			{
-				m_densityMap[m_width*y+x] = 0.5;
+				m_densityMap[m_width*y+x] = offset;
 				m_densityMap[m_width*y+x]  += amplitude - GaussAtCoordinate(Math::Vec2(x,y),Math::Vec2(m_width/2,m_height/2),Math::Vec2(m_width/2,m_height/2),amplitude);
 				/*m_densityMap[m_width*y+x]  += GaussAtCoordinate(Math::Vec2(x,y),Math::Vec2(m_width/4,m_height/2),Math::Vec2(50,30),3);
 				m_densityMap[m_width*y+x]  += GaussAtCoordinate(Math::Vec2(x,y),Math::Vec2(500,m_height/3),Math::Vec2(200,30),-0.5);
 				m_densityMap[m_width*y+x]  += g_rand->Uniform(-0.4f,0.5f);*/
 				//m_densityMap[m_width*y+x] += (sin(sqrt((m_width-x)*(m_width-x))*0.03)) *0.25; //+ cos((m_height-y)*(m_height-y))) * 0.5;
 				//m_densityMap[m_width*y+x] += (cos(sqrt((m_height-y)*(m_height-y))*0.05)) *0.25;
+				m_densityMap[m_width*y+x]  /= amplitude + offset;
 			} // for x
 		} // for y
 	}
